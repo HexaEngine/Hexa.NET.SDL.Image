40fd43536785ed09dbb4045993fa9b2742736785
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-08-14 15:57:04 +0200

    Added binaries and readme and license

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -2,6 +2,5 @@
 using HexaGen;
 
 CsCodeGeneratorSettings settings = CsCodeGeneratorSettings.Load("generator.json");
-settings.SystemIncludeFolders.Add("sdl");
 CsCodeGenerator generator = new(settings);
-generator.Generate(new List<string>() { "include/SDL_image.h" }, "../../../../Hexa.NET.SDL2.Image/Generated");
+generator.Generate(["include/SDL_image.h"], "../../../../Hexa.NET.SDL2.Image/Generated");
